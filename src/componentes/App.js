db33ef6dad5238348cd535f2ac1ad91ba48e9ea4
Edu <edumartsil95@tutanota.com>
2021-03-09 18:42:52 +0100

    Creado base de la app

---
+++ src/componentes/App.js
@@ -0,0 +1,22 @@
+import '../css/App.css';
+import Header from "./Header";
+
+function App() {
+    return (
+        <div className="App container">
+            <Header titulo='Gasto semanal'/>
+            <div className='contenido-principal contenido'>
+                <div className='row'>
+                    <div className='one-half column'>
+
+                    </div>
+                    <div className='one-half column'>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
